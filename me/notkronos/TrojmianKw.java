87e36888e73c011747d65e8bb67475f89bdc7922
jan <jan.kuczaj.2022@zsa.pwr.edu.pl>
2022-10-06 10:36:34 +0000

    finished UI

--- me/notkronos/TrojmianKw.java
+++ me/notkronos/TrojmianKw.java
@@ -10,7 +10,7 @@ public class TrojmianKw extends JFrame implements ActionListener {
 	private static final long serialVersionUID = 1L;
 
 	JButton button;
-	JTextField[] textFields = new JTextField[11];
+	JTextField[] textFields = new JTextField[12];
 	JLabel polecenie, a, b, c, d;
 	public TrojmianKw() {
 		super();
@@ -31,6 +31,78 @@ public class TrojmianKw extends JFrame implements ActionListener {
 		textFields[0] = new JTextField();
 		textFields[0].setBounds(30, 80, 40, 15);
 		this.add(textFields[0]);
+		setVisible(true);
+
+		textFields[1] = new JTextField();
+		textFields[1].setBounds(90, 80, 40, 15);
+		this.add(textFields[1]);
+		setVisible(true);
+
+		textFields[2] = new JTextField();
+		textFields[2].setBounds(150, 80, 40, 15);
+		this.add(textFields[2]);
+		setVisible(true);
+
+		b = new JLabel("(x + 2)(x + 8) = ");
+		b.setBounds(30, 120, 400, 80);
+		this.add(b);
+
+		textFields[3] = new JTextField();
+		textFields[3].setBounds(30, 170, 40, 15);
+		this.add(textFields[3]);
+		setVisible(true);
+
+		textFields[4] = new JTextField();
+		textFields[4].setBounds(90, 170, 40, 15);
+		this.add(textFields[4]);
+		setVisible(true);
+
+		textFields[5] = new JTextField();
+		textFields[5].setBounds(150, 170, 40, 15);
+		this.add(textFields[5]);
+		setVisible(true);
+
+		c = new JLabel("(x - 3)(x + 6) = ");
+		c.setBounds(30, 210, 400, 80);
+		this.add(c);
+
+		textFields[6] = new JTextField();
+		textFields[6].setBounds(30, 260, 40, 15);
+		this.add(textFields[6]);
+		setVisible(true);
+
+		textFields[7] = new JTextField();
+		textFields[7].setBounds(90, 260, 40, 15);
+		this.add(textFields[7]);
+		setVisible(true);
+
+		textFields[8] = new JTextField();
+		textFields[8].setBounds(150, 260, 40, 15);
+		this.add(textFields[8]);
+		setVisible(true);
+
+		d = new JLabel("(x - 7)(x - 3) = ");
+		d.setBounds(30, 300, 400, 80);
+		this.add(d);
+
+		textFields[9] = new JTextField();
+		textFields[9].setBounds(30, 350, 40, 15);
+		this.add(textFields[9]);
+		setVisible(true);
+
+		textFields[10] = new JTextField();
+		textFields[10].setBounds(90, 350, 40, 15);
+		this.add(textFields[10]);
+		setVisible(true);
+
+		textFields[11] = new JTextField();
+		textFields[11].setBounds(150, 350, 40, 15);
+		this.add(textFields[11]);
+
+		button  = new JButton("Check");
+		button.setBounds(66, 400, 80, 25);
+		this.add(button);
+
 		setVisible(true);
 	}
 	
